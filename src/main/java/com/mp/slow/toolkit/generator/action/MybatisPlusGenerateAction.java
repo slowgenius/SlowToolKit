f0e3ae353d095f123b5f4d36fee6467d0d74cfa5
笨天才 <305109030@qq.com>
2022-06-27 21:53:33 +0800

    生成mybatis-plus注解最终版 ，解决所有问题

--- src/main/java/com/mp/slow/toolkit/generator/action/MybatisPlusGenerateAction.java
+++ src/main/java/com/mp/slow/toolkit/generator/action/MybatisPlusGenerateAction.java
@@ -4,25 +4,23 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.PsiAnnotation;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementFactory;
 import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiImportStatement;
-import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.psi.codeStyle.CodeStyleManager;
-import com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl;
-import com.intellij.psi.javadoc.PsiDocComment;
+import com.intellij.psi.codeStyle.JavaCodeStyleManager;
 import com.mp.slow.toolkit.utils.StrUtils;
 import org.jetbrains.annotations.NotNull;
-import org.junit.Test;
 
 import java.util.Objects;
 
-public class Vo2DtoGenerateAction extends AnAction {
+public class MybatisPlusGenerateAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
@@ -44,12 +42,16 @@ public class Vo2DtoGenerateAction extends AnAction {
                 }
                 Objects.requireNonNull(field.getModifierList()).addAnnotation(anno);
             }
-
             PsiImportStatement importStatementOnDemand = elementFactory.createImportStatementOnDemand("com.baomidou.mybatisplus.annotation");
             psiClass.getParent().addBefore(importStatementOnDemand, psiClass);
-            //elementFactory.createClass("com.baomidou.mybatisplus.annotation");
+            Document document = Objects.requireNonNull(event.getData(CommonDataKeys.EDITOR)).getDocument();
+            PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(document);
+            document.insertString(psiClass.getParent().getChildren()[1].getTextOffset(), "import com.baomidou.mybatisplus.annotation.TableId;\nimport com.baomidou.mybatisplus.annotation.TableField;");
+            PsiDocumentManager.getInstance(project).commitAllDocuments();
 
             CodeStyleManager.getInstance(project).reformat(psiClass);
+            JavaCodeStyleManager.getInstance(project).optimizeImports(Objects.requireNonNull(event.getData(CommonDataKeys.PSI_FILE)));
+
         });
 
     }
@@ -65,14 +67,6 @@ public class Vo2DtoGenerateAction extends AnAction {
             }
         }
         return (PsiClass) data;
-    }
-
-    @Test
-    public void test() {
-        PsiWhiteSpace psiWhiteSpace = new PsiWhiteSpaceImpl("\n");
-
-        PsiElement parent = psiWhiteSpace.getParent();
-        System.out.println(parent);
     }
 
 }
