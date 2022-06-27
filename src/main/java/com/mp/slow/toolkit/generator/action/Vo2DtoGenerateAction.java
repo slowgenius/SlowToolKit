91a0ee7390d795eebe8cebdbc8f10ec6b2c9243e
hantianyi <hantianyi@01lb.com.cn>
2022-06-27 15:19:57 +0800

    稍微修复一下注解生成位置不对的问题

--- src/main/java/com/mp/slow/toolkit/generator/action/Vo2DtoGenerateAction.java
+++ src/main/java/com/mp/slow/toolkit/generator/action/Vo2DtoGenerateAction.java
@@ -12,8 +12,13 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementFactory;
 import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiImportStatement;
+import com.intellij.psi.PsiWhiteSpace;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl;
+import com.intellij.psi.javadoc.PsiDocComment;
 import com.mp.slow.toolkit.utils.StrUtils;
 import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
 
 import java.util.Objects;
 
@@ -22,35 +27,58 @@ public class Vo2DtoGenerateAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
         PsiElement data = Objects.requireNonNull(event.getData(CommonDataKeys.PSI_FILE)).findElementAt(Objects.requireNonNull(event.getData(CommonDataKeys.EDITOR)).getCaretModel().getOffset());
-        //PsiElement data = event.getDataContext().getData(CommonDataKeys.PSI_ELEMENT);
-        PsiClass psiClass;
+        PsiClass psiClass = getPsiClass(data);
+        assert psiClass != null;
+        Project project = event.getProject();
+        assert project != null;
+        PsiElementFactory elementFactory = JavaPsiFacade.getInstance(project).getElementFactory();
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            for (PsiField field : psiClass.getFields()) {
+                PsiAnnotation anno;
+                if (field.getName().equals("id")) {
+                    anno = elementFactory.createAnnotationFromText("@TableId(\"id\")\n", psiClass);
+                } else {
+                    anno = elementFactory.createAnnotationFromText("@TableField(\"" + StrUtils.camelToUnderline(field.getName()) + "\")\n", psiClass);
+                }
+                int index = -1;
+                PsiElement[] children = field.getChildren();
+                for (int i = 0; i < children.length; i++) {
+                    if (children[i] instanceof PsiDocComment) {
+                        index = i;
+                        break;
+                    }
+                }
+                if (index == -1) {
+                    field.addBefore(anno, field.getFirstChild());
+                } else {
+                    field.addAfter(anno, field.getChildren()[index + 1]);
+                }
+            }
+            PsiImportStatement importStatementOnDemand = elementFactory.createImportStatementOnDemand("com.baomidou.mybatisplus.annotation");
+            psiClass.getParent().addBefore(importStatementOnDemand, psiClass);
+        });
+
+    }
+
+    private PsiClass getPsiClass(PsiElement data) {
         int count = 0;
         while (!(data instanceof PsiClass)) {
             assert data != null;
             data = data.getParent();
             count++;
             if (count > 10) {
-                return;
+                return null;
             }
         }
-        psiClass = (PsiClass) data;
-        Project project = event.getProject();
-        assert project != null;
-        PsiElementFactory elementFactory = JavaPsiFacade.getInstance(project).getElementFactory();
-        PsiClass tableField1 = elementFactory.createAnnotationType("TableField");
-        WriteCommandAction.runWriteCommandAction(project, () -> {
-            for (PsiField field : psiClass.getFields()) {
-                PsiAnnotation anno;
-                if (field.getName().equals("id")) {
-                    anno = elementFactory.createAnnotationFromText("@TableId(\"id\")", tableField1);
-                } else {
-                    anno = elementFactory.createAnnotationFromText("@TableField(\"" + StrUtils.camelToUnderline(field.getName()) + "\")", tableField1);
-                }
-                psiClass.addBefore(anno, field);
-            }
-            PsiImportStatement importStatementOnDemand = elementFactory.createImportStatementOnDemand("com.baomidou.mybatisplus.annotation");
-            psiClass.getParent().addBefore(importStatementOnDemand, psiClass);
-        });
+        return (PsiClass) data;
+    }
+
+    @Test
+    public void test() {
+        PsiWhiteSpace psiWhiteSpace = new PsiWhiteSpaceImpl("\n");
+
+        PsiElement parent = psiWhiteSpace.getParent();
+        System.out.println(parent);
     }
 
 }
