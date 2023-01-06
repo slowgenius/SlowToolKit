f7808ce836bf42f895d224f6e747f85f279344fa
hantianyi <hantianyi@01lb.com.cn>
2023-01-06 11:11:26 +0800

    优化ui界面，支持一下json数组的格式化

--- src/main/java/com/slowgenius/toolkit/toolkit/ui/JsonFormatter.java
+++ src/main/java/com/slowgenius/toolkit/toolkit/ui/JsonFormatter.java
@@ -11,6 +11,9 @@ import com.slowgenius.toolkit.utils.JSONUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.KeyAdapter;
@@ -29,6 +32,8 @@ public class JsonFormatter {
     private JTextArea text;
     private JTextField jsonPath;
     private JButton submit;
+    private JPanel inputs;
+    private JPanel results;
     private final Project project;
 
 
@@ -46,6 +51,22 @@ public class JsonFormatter {
         text.setLineWrap(true);
         text.setWrapStyleWord(true);
         submit.addActionListener(action -> formatAction());
+        main.addComponentListener(new ComponentAdapter() {
+            /**
+             * Invoked when the component's size changes.
+             *
+             * @param e
+             */
+            @Override
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
+                Dimension mainSize = main.getSize();
+                inputs.setMinimumSize(new Dimension(Double.valueOf(mainSize.getWidth() - 150).intValue() / 2, -1));
+                inputs.setMaximumSize(new Dimension(Double.valueOf(mainSize.getWidth() - 150).intValue() / 2, -1));
+                results.setMinimumSize(new Dimension(Double.valueOf(mainSize.getWidth() - 150).intValue() / 2, -1));
+                results.setMaximumSize(new Dimension(Double.valueOf(mainSize.getWidth() - 150).intValue() / 2, -1));
+            }
+        });
     }
 
     public void formatAction() {
@@ -57,7 +78,14 @@ public class JsonFormatter {
         if (StringUtils.isNotBlank(jsonPath.getText())) {
             unFormatJsonObject = JSONUtils.read(unFormatJson, jsonPath.getText());
         } else {
-            unFormatJsonObject = JSONObject.parseObject(unFormatJson);
+            unFormatJson = unFormatJson.trim();
+            if (unFormatJson.startsWith("{")) {
+                unFormatJsonObject = JSONObject.parseObject(unFormatJson);
+            } else if (unFormatJson.startsWith("[")) {
+                unFormatJsonObject = JSONObject.parseArray(unFormatJson);
+            }else {
+                unFormatJsonObject = "json格式有误";
+            }
         }
         String resultJson = JSON.toJSONString(unFormatJsonObject, SerializerFeature.PrettyFormat).replace("\t", "    ");
         result.setText(resultJson);
@@ -70,5 +98,11 @@ public class JsonFormatter {
 
     public Project getProject() {
         return project;
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        inputs = new JPanel();
+        results = new JPanel();
     }
 }
