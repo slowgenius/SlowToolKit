0e339384bff1293f3c5934ca2969908a1e779e29
hantianyi <hantianyi@01lb.com.cn>
2023-03-29 21:12:45 +0800

    create class by json schema

--- src/main/java/com/slowgenius/toolkit/autoCreate/CreateClasses.kt
+++ src/main/java/com/slowgenius/toolkit/autoCreate/CreateClasses.kt
@@ -11,14 +11,12 @@ import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.InputValidatorEx
 import com.intellij.openapi.util.text.StringUtil
-import com.intellij.psi.JavaDirectoryService
-import com.intellij.psi.PsiDirectory
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiElementFactory
+import com.intellij.psi.*
 import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.util.PlatformIcons
 import com.slowgenius.toolkit.base.SlowAbstractAction
 import com.slowgenius.toolkit.utils.SlowPsiUtils
+import com.slowgenius.toolkit.utils.SlowStrUtils
 import com.slowgenius.toolkit.utils.Utils
 import org.apache.commons.lang3.StringUtils
 import java.util.*
@@ -47,24 +45,36 @@ open class CreateClasses : SlowAbstractAction() {
                         Arrays.stream(name.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray())
                             .collect(Collectors.toList())
                     else listOf(name)
+
                 //循环创建类文件
                 val result = classNameList.stream()
                     .map { className: String? ->
                         JavaDirectoryService.getInstance().createClass(dir, className!!, templateName, true)
                     }.collect(Collectors.toList())
-                //返回第一个元素
                 val psiClass = result[0];
+
+
+                //返回第一个元素
                 val systemClipboardText = Utils.getSystemClipboardText().trim()
                 if (!StringUtils.startsWith(systemClipboardText, "{")) {
                     return psiClass
                 }
                 val parseObject = JSONObject.parseObject(systemClipboardText);
+                //是json-sechma
+                if (parseObject.getJSONArray("required") != null && parseObject.getJSONObject("properties") != null) {
+                    WriteCommandAction.writeCommandAction(project).run<RuntimeException> {
+                        genClassByJson(psiClass, dir, parseObject, project)
+                        CodeStyleManager.getInstance(project).reformat(psiClass)
+                    }
+                    return psiClass
+                }
                 WriteCommandAction.writeCommandAction(project).run<RuntimeException> {
                     parseObject.keys.forEach { item: String ->
                         val createPrimitiveType = PsiElementFactory.getInstance(project)
                             .createType(SlowPsiUtils.getClassByName(project, "java.lang.String"))
                         val field = PsiElementFactory.getInstance(project).createField(item, createPrimitiveType)
-                        val comment = PsiElementFactory.getInstance(project).createDocCommentFromText("\n/**\n * \n */")
+                        val comment =
+                            PsiElementFactory.getInstance(project).createDocCommentFromText("\n/**\n * \n */")
                         field.modifierList?.addBefore(comment, field.modifierList?.firstChild)
                         psiClass.add(field)
                         CodeStyleManager.getInstance(project).reformat(psiClass)
@@ -85,6 +95,38 @@ open class CreateClasses : SlowAbstractAction() {
             if (createdElement != null) view.selectElement(createdElement)
         }
 
+    }
+
+    public fun genClassByJson(psiClass: PsiClass, dir: PsiDirectory, json: JSONObject, project: Project) {
+
+        val jsonObject = json.getJSONObject("properties")
+        jsonObject.keys.forEach { item: String ->
+            val field: PsiField
+            if (jsonObject.getJSONObject(item).getString("type").equals("object")) {
+                val className = SlowStrUtils.firstLetterUpper(item)
+                val createClass = JavaDirectoryService.getInstance().createClass(dir, className, "Class", true)
+                genClassByJson(createClass, dir, jsonObject.getJSONObject(item), project)
+                val createPrimitiveType = PsiElementFactory.getInstance(project)
+                    .createType(SlowPsiUtils.getClassByName(project, createClass.qualifiedName!!))
+                 field = PsiElementFactory.getInstance(project).createField(item, createPrimitiveType)
+                //添加import
+                val document = PsiDocumentManager.getInstance(project).getDocument( psiClass.containingFile)
+                PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(document!!)
+                document.insertString(psiClass.parent.children[1].textOffset, "import ${createClass.qualifiedName};\n")
+                //提交
+                PsiDocumentManager.getInstance(project).commitAllDocuments()
+
+            } else {
+                val createPrimitiveType = PsiElementFactory.getInstance(project)
+                    .createType(SlowPsiUtils.getClassByName(project, "java.lang.String"))
+                 field = PsiElementFactory.getInstance(project).createField(item, createPrimitiveType)
+            }
+            val description = jsonObject.getJSONObject(item).getString("description")
+            val comment = PsiElementFactory.getInstance(project)
+                .createDocCommentFromText("\n/**\n * $description\n */")
+            field.modifierList?.addBefore(comment, field.modifierList?.firstChild)
+            psiClass.add(field)
+        }
     }
 
     private fun buildDialog(project: Project, builder: CreateFileFromTemplateDialog.Builder) {
